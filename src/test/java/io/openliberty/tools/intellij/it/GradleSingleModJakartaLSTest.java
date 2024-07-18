f0db7b3ddbfcb4dca085e98d9222e3e1eb93ea05
vaisakhkannan <133869023+vaisakhkannan@users.noreply.github.com>
2024-07-18 15:28:05 +0530

    just

--- src/test/java/io/openliberty/tools/intellij/it/GradleSingleModJakartaLSTest.java
+++ src/test/java/io/openliberty/tools/intellij/it/GradleSingleModJakartaLSTest.java
@@ -11,11 +11,13 @@ package io.openliberty.tools.intellij.it;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 
 import java.nio.file.Paths;
 
 //@DisabledOnOs({OS.WINDOWS})
+@EnabledOnOs({OS.WINDOWS})
 public class GradleSingleModJakartaLSTest extends SingleModJakartaLSTestCommon {
 
     /**
