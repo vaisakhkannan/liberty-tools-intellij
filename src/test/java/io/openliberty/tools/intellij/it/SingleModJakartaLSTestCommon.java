5311c95061640fb49c19cc15b4ab72209de273e8
vaisakhkannan <VAISAKH.T@ibm.com>
2025-03-12 00:00:00 +0530

    Added loop to check the correct diagnostic

--- src/test/java/io/openliberty/tools/intellij/it/SingleModJakartaLSTestCommon.java
+++ src/test/java/io/openliberty/tools/intellij/it/SingleModJakartaLSTestCommon.java
@@ -125,6 +125,16 @@ public abstract class SingleModJakartaLSTestCommon {
             UIBotTestUtils.hoverInAppServerCfgFile(remoteRobot, flaggedString, "SystemResource2.java", UIBotTestUtils.PopupType.DIAGNOSTIC);
 
             String foundHoverData = UIBotTestUtils.getHoverStringData(remoteRobot, UIBotTestUtils.PopupType.DIAGNOSTIC);
+
+            // if the LS has not yet poulated the popup, re-get the popup data
+            for (int i = 0; i<=5; i++){
+                if (foundHoverData.contains("method 'getProperties' is never used")) {
+                    foundHoverData = UIBotTestUtils.getHoverStringData(remoteRobot, UIBotTestUtils.PopupType.DIAGNOSTIC);
+                }
+                else {
+                    break;
+                }
+            }
             TestUtils.validateHoverData(expectedHoverData, foundHoverData);
             UIBotTestUtils.clickOnFileTab(remoteRobot, "SystemResource2.java");
 
